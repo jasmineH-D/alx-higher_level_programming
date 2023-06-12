9692569b7cf0aca008183c67ada8a70fbbfb36fb
jasmineH-D <mellyasminita2000@gmail.com>
2023-06-12 04:24:38 -0700

    tasks

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,22 +1,64 @@
-#include <listobject.h>
-#include <object.h>
-#include <Python.h>
+#include "lists.h"
 
 /**
- * print_python_list_info - function that prints info about python lists
- * @p: python lisze
- * Return: void
+ * reverse_listint - reverses single linked list
+ * @head: pointer to head of the list
+ * Return: pointer to head
  */
 
-void print_python_list_info(PyObject *p)
+listint_t *reverse_listint(listint_t **head)
 {
-	int i;
-	long int size = PyList_Size(p);
-	PyListObject *obj = (PyListObject *)p;
+	listint_t *node = *head, *next, *prev = NULL;
 
-	printf("[*] Size of the Python List = %li\n", size);
-	printf("[*] Allocated = %li\n", obj->allocated);
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+	*head = prev;
+	return (*head);
+}
 
-	for (i = 0; i < size; i++)
-		print("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to head of the linked list
+ * Return: if it's palindrome return 0, otherwise returns 1
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+	tmp = *head;
+	while (tmp)
+	{
+		size++;
+		tmp = tmp->next;
+	}
+
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
+	{
+		if (tmp->n != rev->n)
+			return (0);
+		tmp = tmp->next;
+		rev = rev->next;
+	}
+	reverse_listint(&mid);
+	return (1);
 }
--- 8-multiple_returns.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def multiple_returns(sentence):
-    first = ''
-    length = len(sentence)
-    if sentence == '':
-        first = None
-    else:
-        first = sentence[0]
-    return length, first
