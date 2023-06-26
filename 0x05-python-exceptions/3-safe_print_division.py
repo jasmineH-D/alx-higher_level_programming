72a7bb6f84776c45bfeabe25ed95583838a54a90
jasmineH-D <mellyasminita2000@gmail.com>
2023-06-26 15:42:03 -0700

    tasks

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    resultt = 0
+    try:
+         resultt = a / b
+    except ZeroDivisionError:
+         resultt = None
+    finally:
+        print("Inside result: {}".format(resultt))
+        return resultt
