1670d2d4ec2bc403cb5e9041708487c59185294e
jasmineH-D <mellyasminita2000@gmail.com>
2023-06-27 16:10:29 -0700

    tasks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Defining new class Square"""
+
+
 class Square:
      """class Square
      Attributes:
@@ -10,6 +12,7 @@ class Square:
      def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -17,13 +20,16 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     @property
     def size(self):
          """gets value of size"""
          return self.__size
+
     @property
     def position(self):
          return self.__position
+
     @position.setter
     def position(self, value):
         if type(value) is not tuple or len(value) != 2 or \
@@ -32,12 +38,15 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
+
     def area(self):
         return (self.__size * self.__size)
+
     def my_print(self):
         if self.__size == 0:
              print()
              return
+
          for i in range(0, self.__position[1]):
              print()
         for j in range(self.__size):
