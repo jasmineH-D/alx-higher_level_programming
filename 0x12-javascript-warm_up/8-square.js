cab0247614fd9ad9eaf479e4c43ebcd39e719e86
jasmineH-D <mellyasminita2000@gmail.com>
2023-09-12 14:07:23 -0700

    tasks

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let col = 0; col < size; col++) row += 'X';
+    console.log(row);
+  }
+}
