4af28547da2aeeb9122e97e597e1da9da23bea15
yasmine haddad <122939862+jasmineH-D@users.noreply.github.com>
2023-10-24 23:46:12 +0100

    tasks

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
+/* script that writes a string to a file. */
 
 const fs = require('fs');
-
-try {
-  fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err, result) { if (err) console.log(err); });
-} catch (err) {
-  console.log(err);
-}
+const fn = process.argv[2];
+const txt = process.argv[3];
+fs.writeFile(fn, txt, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
